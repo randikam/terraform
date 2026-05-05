da4eacdf2dcf2f77f610908a6f22f32e9364aacb
randikam <randika.sliit@gmail.com>
2026-05-05 16:39:42 +1000

    Create provider.tf

---
+++ test_ec2/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.10"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"   # Change if needed
+  profile = "tabdev-developer-158428591747"
+}
